fc1ee4494e521c9a064fa54a8cf08ba7b4cda714
Amos <amosmakupe8@gmail.com>
2024-08-13 22:38:01 +0200

    Fixing build error

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(json());
 app.use('/api', apiRouter);
 
 app.use(express.static(path.join(__dirname,'client', 'build')))
-app.get('/*', (req, res)=>{
+app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname,'client','build','index.html'), (err)=>{
         if(err){
             res.status(500).send(err)
